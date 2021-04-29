228a9a00811973a76af48d85f6414b4959192b25
Jeremie Maufret <sayou.jeremie@gmail.com>
2021-04-29 22:09:22 +0200

    feature array algo python

--- python/algorithms.py
+++ python/algorithms.py
@@ -1,4 +1,4 @@
-def allCombinations(array: list, begin: int = 0):
+def allCombinations(array: list, begin: int = 0, subarray: bool = False):
     temp = []
     if len(array) == 0: return []
     else:
@@ -6,21 +6,28 @@ def allCombinations(array: list, begin: int = 0):
             valueBinary = str(format(i, 'b'))
             while len(valueBinary) < len(array):
                 valueBinary = ''.join(['0', valueBinary])
-            value = ''
+            if subarray: value = []
+            else: value = ''
             for a in range(len(valueBinary)):
                 if list(reversed(valueBinary))[a] == '1':
-                    value += str(array[a])
-            temp.append(value)
+                    if subarray: value.append(array[a])
+                    else: value += str(array[a])
+            if subarray: temp.append(value)
+            else: temp.append(value)
         return(temp)
+        
 
 ##################################
 # The function returns the combination of all possibilities
 # The second parameter which is optional is the pitch, by default it starts at 0
+# The third parameter is there for the distinction if it is a string or an array
 # Tests
 # print(allCombinations([0, 1, 2, 3], 1))
 # -> ['0', '1', '01', '2', '02', '12', '012', '3', '03', '13', '013', '23', '023', '123', '0123']
 # print(allCombinations(['a', 'b', 'c']))
 # -> ['', 'a', 'b', 'ab', 'c', 'ac', 'bc', 'abc']
+# print(allCombinations(['a', 'b', 'c'], 1, True))
+# -> [['a'], ['b'], ['a', 'b'], ['c'], ['a', 'c'], ['b', 'c'], ['a', 'b', 'c']]
 # print(allCombinations([]))
 # -> []
 ##################################
@@ -52,6 +59,8 @@ def mathCombinations(array: list, duplicate: bool = False):
 # -> [1, 2, 3, 4, 5, 6, 8, 10, 12, 15, 20, 24, 30, 40, 60, 120]
 # print(mathCombinations(allCombinations([1, 2, 3, 4, 5], 1), True))
 # -> [1, 2, 2, 3, 3, 6, 6, 4, 4, 8, 8, 12, 12, 24, 24, 5, 5, 10, 10, 15, 15, 30, 30, 20, 20, 40, 40, 60, 60, 120, 120]
+# print(mathCombinations(allCombinations([1, 2, 3, 5, 7, 11], 1), False))
+# -> [1, 2, 3, 5, 6, 7, 10, 14, 15, 21, 30, 35, 42, 70, 105, 210]
 # print(mathCombinations(allCombinations([])))
 # -> []
 ##################################
